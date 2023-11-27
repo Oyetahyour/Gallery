d2cdcec021b6c471414e6ffc7a07517ee262da0d
Oyetahyour <oyetayoyekeen01@gmail.com>
2023-11-27 21:24:59 +0100

    Cycling Gallery

---
+++ script.js
@@ -0,0 +1,60 @@
+const Nigeria = document.getElementById ("Image 1")
+Nigeria.addEventListener("click",(event) =>{
+   event.target.style.background = "black"
+   event.target.style.color = "white"
+} )
+const Nigeria2 = document.getElementById ("Image 2")
+Nigeria2.addEventListener("click", (event) => {
+    event.target.style.background ="Green"
+    event.target.style.color ="black"
+})
+const Nigeria3 = document.getElementById ("Image 3")
+Nigeria3.addEventListener("click", (event) => {
+    event.target.style.background ="purple"
+    event.target.style.color ="cyan"
+})
+const Nigeria4 = document.getElementById ("Image 4")
+Nigeria4.addEventListener("click", (event) => {
+    event.target.style.background ="yellow"
+    event.target.style.color ="blue"
+})
+const Nigeria5 = document.getElementById ("Image 5")
+Nigeria5.addEventListener("click", (event) => {
+    event.target.style.background ="brown"
+    event.target.style.color ="black"
+})
+const Nigeria6 = document.getElementById ("Image 6")
+Nigeria6.addEventListener("click", (event) => {
+    event.target.style.background ="orange"
+   event.target.style.color ="navy"
+})
+const Nigeria7 = document.getElementById ("Image 7")
+Nigeria7.addEventListener("click", (event) => {
+    event.target.style.background ="red"
+    event.target.style.color ="white"
+})
+const Nigeria8 = document.getElementById ("Image 8")
+Nigeria8.addEventListener("click", (event) => {
+    event.target.style.background ="Green"
+    event.target.style.color ="purple"
+})
+const Nigeria9 = document.getElementById ("Image 9")
+Nigeria9.addEventListener("click", (event) => {
+    event.target.style.background ="aqua"
+    event.target.style.color ="blue"
+})
+const Nigeria10 = document.getElementById ("Image 10")
+Nigeria10.addEventListener("click", (event) => {
+    event.target.style.background ="navy"
+    event.target.style.color ="red"
+})
+const Nigeria11 = document.getElementById ("Image 11")
+Nigeria11.addEventListener("click", (event) => {
+    event.target.style.background ="lime"
+    event.target.style.color ="yellow"
+})
+const Nigeria12 = document.getElementById ("Image 12")
+Nigeria12.addEventListener("click", (event) => {
+    event.target.style.background ="teal"
+    event.target.style.color ="silver"
+})
